9b60efefbf24b9b38cfce0c7863e2da9777e6ba1
unknown <truongthienloc2@gmail.com>
2023-12-14 22:14:27 +0700

    Set up - Config tailwind and alias

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,24 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      ["module-resolver", {
-        "~/": "./src/",
-        "@assets/": "./assets/"
-      }]
-    ]
-  };
-};
+module.exports = function (api) {
+    api.cache(true)
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: [
+            [
+                'module-resolver',
+                {
+                    root: ['.'],
+                    extensions: [
+                        '.js', '.jsx', '.json', '.png', '.jpg'
+                    ],
+                    alias: {
+                        '~': './src',
+                        '@assets': './assets',
+                    },
+                    // "~/": "./src/",
+                    // "@assets/": "./assets/"
+                },
+            ],
+            'nativewind/babel',
+        ],
+    }
+}
