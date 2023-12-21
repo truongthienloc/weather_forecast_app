7f167890fb9175300d732631fc2ebb97948b621e
nHuy101 <nhathuyho2k3@gmail.com>
2023-12-21 20:47:08 +0700

    init asset & screen

--- src/screens/CitiesScreen/CitiesScreen.jsx
+++ src/screens/CitiesScreen/CitiesScreen.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import { useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react'
 import { View, Text } from 'react-native'
+import { SearchBar } from '@rneui/themed';
+
 
 const CitiesScreen = () => {
+  const navigation = useNavigation();
+  const [search, setSearch] = useState();
+
+  const updateSearch = (search) => {
+    setSearch(search);
+  };
+
+  useEffect(() => {
+    navigation.setOptions({
+      title: 'Quản lý thành phố',
+    })
+  }, [navigation]);
   return (
     <View>
-      <Text>
-        Add Cities Screen
-      </Text>
+      <SearchBar 
+          lightTheme='true' 
+          placeholder="Tìm kiếm thành phố..."
+          onChangeText={updateSearch}
+          value={search}
+      />
     </View>
   )
 }
--- src/screens/SettingScreen/SettingScreen.jsx
+++ src/screens/SettingScreen/SettingScreen.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { React, useEffect } from 'react'
 import { Text, View } from 'react-native'
 
 const SettingScreen = () => {
+  const navigation = useNavigation();
+  useEffect(() => {
+    navigation.setOptions({
+      title: 'Cài đặt',
+    })
+  }, [navigation]);
   return (
     <View>
       <Text>Setting Screen</Text>
