fe7be3c7d8b00795a978cc8124975671379ae349
nHuy101 <nhathuyho2k3@gmail.com>
2023-12-22 04:08:25 +0700

    region & setting raw ui

--- src/screens/CitiesScreen/CitiesScreen.jsx
+++ src/screens/CitiesScreen/CitiesScreen.jsx
@@ -1,32 +1,53 @@
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native'
 import React, { useEffect, useState } from 'react'
-import { View, Text } from 'react-native'
-import { SearchBar } from '@rneui/themed';
-
+import { View, Text, StyleSheet, ScrollView, FlatList } from 'react-native'
+import { SearchBar } from '@rneui/themed'
+import { CityItem } from '~/components/CityItem'
+import { StatusBar } from 'expo-status-bar'
 
 const CitiesScreen = () => {
-  const navigation = useNavigation();
-  const [search, setSearch] = useState();
+    const navigation = useNavigation()
+    const [search, setSearch] = useState()
+    const updateSearch = (search) => {
+        setSearch(search)
+    }
 
-  const updateSearch = (search) => {
-    setSearch(search);
-  };
-
-  useEffect(() => {
-    navigation.setOptions({
-      title: 'Quản lý thành phố',
-    })
-  }, [navigation]);
-  return (
-    <View>
-      <SearchBar 
-          lightTheme='true' 
-          placeholder="Tìm kiếm thành phố..."
-          onChangeText={updateSearch}
-          value={search}
-      />
-    </View>
-  )
+    useEffect(() => {
+        navigation.setOptions({
+            title: 'Quản lý thành phố',
+        })
+    }, [navigation])
+    return (
+        <>
+            <StatusBar style="light" backgroundColor="black" />
+            <View>
+                <SearchBar
+                    lightTheme="true"
+                    placeholder="Tìm kiếm thành phố..."
+                    onChangeText={updateSearch}
+                    value={search}
+                />
+                <ScrollView style={styles.itemContainer}>
+                  <CityItem location={'Quận 9'} condition={'sunny'}/>
+                  <CityItem location={'Hà Nội'} condition={'rainy'}/>
+                  <CityItem location={'London'} condition={'snowy'}/>
+                  <CityItem location={'New York'} condition={'cloudy'}/>
+                  <CityItem location={'Sydney'} condition={'hot'}/>
+                  <CityItem location={'Quận 9'} condition={'sunny'}/>
+                  <CityItem location={'Hà Nội'} condition={'rainy'}/>
+                  <CityItem location={'London'} condition={'snowy'}/>
+                </ScrollView>
+            </View>
+        </>
+    )
 }
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 10,
+  }
+})
 
 export default CitiesScreen
